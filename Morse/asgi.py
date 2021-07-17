d6cee53df85f0c2ca73e5d6bee12923bbfda1090
Reza Rezaee <rezarezaee.commercial@gmail.com>
2021-07-17 21:28:51 +0430

    imporoved the room name UI

--- Morse/asgi.py
+++ Morse/asgi.py
@@ -1,6 +1,17 @@
-from channels.routing import ProtocolTypeRouter
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from chat import routing as chat_routing
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Morse.settings")
 
 
 application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat_routing.websocket_urlpatterns,
+        )
+    )
 
 })
