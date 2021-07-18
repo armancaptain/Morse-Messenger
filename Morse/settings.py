7e5ab1c59c44ffc8718b18d85a0340341c207156
Reza Rezaee <rezarezaee.commercial@gmail.com>
2021-07-18 17:49:28 +0430

    add redis portable .zip in directory.(run redis on 6370 port OR change redis settings in settings.py)

--- Morse/settings.py
+++ Morse/settings.py
@@ -79,7 +79,7 @@ CHANNEL_LAYERS = {
         # 'BACKEND': 'channels.layers.InMemoryChannelLayer',
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('127.0.0.1', 6370)],
         },
     },
 }
